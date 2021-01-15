898a839c2ad7654343ac166090b07fd8b818b0ce
@shahasad78 <shahasad78@gmail.com>
2021-01-14 21:52:38 -0800

    - created a NextJS blog project - added a custom profile image - created dynamic pre-rendered routes (for posts) - used CSS modules - created a very basic API route - used Link and Head tags from next library - parsed markdown with remark - created a 404 page.

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+        <>
+            <h1>WE ARE FAKE GANGSTERS</h1>
+            <p>We were not Gangstery enough to get you this page. So, being the lazy bastards we are, made this ONE catch-all page.</p>
+        </>
+    )
+}
